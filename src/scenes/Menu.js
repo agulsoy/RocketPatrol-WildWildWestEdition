0580243c3cf86446ef14f3f98c88895352e4cd49
agulsoy <agulsoy@ucsc.edu>
2020-04-19 23:41:22 -0700

    changed word alignment

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -21,7 +21,7 @@ class Menu extends Phaser.Scene {
             fontSize: '27px',
             backgroundColor: '#000000',
             color: '#FFFFFF',
-            align: 'right',
+            align: 'center',
             padding: {
                 top:5,
                 bottom: 5,
